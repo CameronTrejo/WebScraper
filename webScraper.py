e6c76b142630fb22beda92f5460a6648bb18f1ce
CameronTrejo2000 <70658557+CameronTrejo@users.noreply.github.com>
2021-04-21 12:07:35 -0600

    Added Web Sraper usage

--- webScraper.py
+++ webScraper.py
@@ -4,7 +4,9 @@ import re
 title = "<title>"
 end_title = "</title>"
 
-url1 = "http://olympus.realpython.org/profiles/aphrodite"
+# url1 = "http://olympus.realpython.org/profiles/aphrodite"
+# url1 = "http://olympus.realpython.org/profiles/poseidon"
+url1 = "http://olympus.realpython.org/profiles/dionysus"
 
 web_page = urlopen(url1)
 html_bytes = web_page.read()
@@ -13,15 +15,23 @@ html = html_bytes.decode("utf-8")
 print(html)
 
 # <title>Profile: Aphrodite</title> 
-title_index_start = html.find(title)
-print (title_index_start)
+regex_title = "<title.*?>.*?</title.*?>"
+match_results = re.search(regex_title, html, re.IGNORECASE)
+title_regex = match_results.group()
+title_regex = re.sub("<.*?>", "", title_regex)
 
-start_of_profile = title_index_start + len(title)
-print(start_of_profile)
+print(title_regex)
 
-title_index_end = html.find(end_title)
-print(title_index_end)
 
-prfile_name = html[start_of_profile:title_index_end]
-print (prfile_name)
+# title_index_start = html.find(title)
+# print (title_index_start)
+
+# start_of_profile = title_index_start + len(title)
+# print(start_of_profile)
+
+# title_index_end = html.find(end_title)
+# print(title_index_end)
+
+# prfile_name = html[start_of_profile:title_index_end]
+# print (prfile_name)
 
